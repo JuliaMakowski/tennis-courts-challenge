5d95694609ad7de6104fbd758c49a234c4193a42
Julia <juliamakowski@hotmail.com>
2021-09-21 21:47:49 -0300

    complete the swagger

--- src/main/java/com/tenniscourts/reservations/ReservationController.java
+++ src/main/java/com/tenniscourts/reservations/ReservationController.java
@@ -3,8 +3,12 @@ package com.tenniscourts.reservations;
 import com.tenniscourts.config.BaseRestController;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.OK;
+@RestController
 @AllArgsConstructor
+@RequestMapping("/reservation")
 public class ReservationController extends BaseRestController {
 
     private final ReservationService reservationService;
@@ -13,14 +17,20 @@ public class ReservationController extends BaseRestController {
         return ResponseEntity.created(locationByEntity(reservationService.bookReservation(createReservationRequestDTO).getId())).build();
     }
 
+    @GetMapping()
+    @CrossOrigin(origins = "*")
     public ResponseEntity<ReservationDTO> findReservation(Long reservationId) {
         return ResponseEntity.ok(reservationService.findReservation(reservationId));
     }
 
+    @PostMapping("/cancel")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<ReservationDTO> cancelReservation(Long reservationId) {
         return ResponseEntity.ok(reservationService.cancelReservation(reservationId));
     }
 
+    @PostMapping("/reschedule")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<ReservationDTO> rescheduleReservation(Long reservationId, Long scheduleId) {
         return ResponseEntity.ok(reservationService.rescheduleReservation(reservationId, scheduleId));
     }
--- src/main/java/com/tenniscourts/schedules/ScheduleController.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleController.java
@@ -3,29 +3,38 @@ package com.tenniscourts.schedules;
 import com.tenniscourts.config.BaseRestController;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+@RestController
 @AllArgsConstructor
+@RequestMapping("/scheduler")
 public class ScheduleController extends BaseRestController {
 
     private final ScheduleService scheduleService;
 
     //TODO: implement rest and swagger
+    @PostMapping("/add")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Void> addScheduleTennisCourt(CreateScheduleRequestDTO createScheduleRequestDTO) {
         return ResponseEntity.created(locationByEntity(scheduleService.addSchedule(createScheduleRequestDTO.getTennisCourtId(), createScheduleRequestDTO).getId())).build();
     }
 
     //TODO: implement rest and swagger
+    @GetMapping("/date")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<List<ScheduleDTO>> findSchedulesByDates(LocalDate startDate,
                                                                   LocalDate endDate) {
         return ResponseEntity.ok(scheduleService.findSchedulesByDates(LocalDateTime.of(startDate, LocalTime.of(0, 0)), LocalDateTime.of(endDate, LocalTime.of(23, 59))));
     }
 
     //TODO: implement rest and swagger
+    @GetMapping()
+    @CrossOrigin(origins = "*")
     public ResponseEntity<ScheduleDTO> findByScheduleId(Long scheduleId) {
         return ResponseEntity.ok(scheduleService.findSchedule(scheduleId));
     }
